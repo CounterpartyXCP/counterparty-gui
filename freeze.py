46281fe6bcf2fcb43556aaa887343ceb79c9a01a
Ouziel Slama <lightzarlboro@gmail.com>
2015-02-24 13:50:57 +0100

    Add desktop and start menu shortcuts

--- freeze.py
+++ freeze.py
@@ -51,12 +51,45 @@ for dll in ['d3dcompiler_47.dll', 'libEGL.dll', 'libGLESv2.dll']:
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+
+shortcut_table = [(
+    "DesktopShortcut",                  # Shortcut
+    "DesktopFolder",                    # Directory_
+    "Counterparty GUI",                 # Name
+    "TARGETDIR",                        # Component_
+    "[TARGETDIR]counterparty-gui.exe",  # Target
+    None,                               # Arguments
+    None,                               # Description
+    None,                               # Hotkey
+    None,                               # Icon
+    0,                                  # IconIndex
+    None,                               # ShowCmd
+    'TARGETDIR'                         # WkDir
+), (
+    "ProgramMenuShortcut",              # Shortcut
+    "ProgramMenuFolder",                # Directory_
+    "Counterparty GUI",                 # Name
+    "TARGETDIR",                        # Component_
+    "[TARGETDIR]counterparty-gui.exe",  # Target
+    None,                               # Arguments
+    None,                               # Description
+    None,                               # Hotkey
+    None,                               # Icon
+    0,                                  # IconIndex
+    None,                               # ShowCmd
+    'TARGETDIR'                         # WkDir
+)]
+
+bdist_msi_options = {'data': {"Shortcut": shortcut_table}}
     
 setup_options = {
     'name': counterpartygui.APP_NAME,
     'version': counterpartygui.APP_VERSION,
-    'options': {"build_exe": build_exe_options},
-    'executables': [Executable("counterparty-gui.py", base=base)]
+    'options': {
+        "build_exe": build_exe_options,
+        "bdist_msi": bdist_msi_options
+    },
+    'executables': [Executable("counterparty-gui.py", base=base, icon="assets/counterparty.ico")]
 }
 
 setup(**setup_options)
